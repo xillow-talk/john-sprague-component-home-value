99e94e8b28d02c76ba92d7a055a55af919613240
kevinbece5 <kevinbece5@gmail.com>
2019-02-06 19:27:33 -0800

    style changes

---
+++ client/style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const CommentContainer = styled.div`
+    display: flex;
+    font-family: sans-serif;
+    font-size: 12px;
+    padding: 12px;
+`;
+
+export const Container = styled.div`
+    flex-direction: column;
+    display: flex;
+`;
+
+export const TimeStamp = styled.span`
+    padding: 5px;
+    display: flex;
+    width: 23%;
+    justify-content: flex-end;
+`;
+
+export const Comment = styled.span`
+    padding-left: 9px;
+    font-family: sans-serif;
+    font-size: 12px;
+`;
