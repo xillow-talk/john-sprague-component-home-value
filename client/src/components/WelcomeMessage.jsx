076a88f050f96d9f1be5da514799d7bad3aeb22c
wizard-jS <spraguej624@gmail.com>
2019-02-15 23:05:36 -0800

    Add react/enzyme dependencies and configuration

---
+++ client/src/components/WelcomeMessage.jsx
@@ -0,0 +1,28 @@
+
+import React, { Fragment } from 'react';
+
+const styles = {
+  heading: {
+    color: '#fff',
+    textAlign: 'center',
+    marginBottom: theme.margin * 2,
+  },
+  logo: {
+    width: 250,
+    heading: 250,
+    objectFit: 'cover',
+  },
+};
+
+const WelcomeMessage = (  ) => {
+  return (
+    <Fragment>
+      <h1 style={}>
+        Welcome To
+      </h1>
+      <img src='' alt="app logo"/>
+    </Fragment>
+  );
+};
+
+export default WelcomeMessage;
