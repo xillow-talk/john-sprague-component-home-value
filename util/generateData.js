7c862891ea315f1fbb15b7db195effb792989f84
wizard-jS <spraguej624@gmail.com>
2019-03-11 15:40:39 -0700

    Fix typo to generate data on aws

--- util/generateData.js
+++ util/generateData.js
@@ -4,7 +4,7 @@ const loremIpsum = require('lorem-ipsum');
 const moment = require('moment');
 
 //  {flags: 'a'}
-const wstream = fs.createWriteStream('../../dummyData/dummyRecords.csv', {flags: 'a'});
+const wstream = fs.createWriteStream('../../dummyData/dummyRecords.csv');
 
 const getRandomTime = () => {
   const randomNum = () => Math.floor(Math.random() * 30 + 1);
@@ -23,7 +23,10 @@ const randomSongNum = () => Math.floor(Math.random() * 10000000);
 
 let string = '';
 
-for (let i = 20000000; i <= 30000000; i++) {
+const newHead = 'profilePic,songId,username,message,postedAt,songTime,followers,\n';
+wstream.write(newHead);
+
+for (let i = 1 ; i <= 35000000; i++) {
   string += `https:s3.amazonaws.com/cloud-vibes-comments/large${randomNum()}.jpg,${randomSongNum()},songname${randomSongNum()},${loremIpsum({ count: 1, units: 'words' })},${loremIpsum()},${getRandomTime()},${randomSongTime()},${randomFollowers()}\n`;
   if (i % 100000 === 0) {
     wstream.write(string);
