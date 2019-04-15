a6a2ab0d853e393ff5fbedc5923b19132b4a3570
wizard-jS <spraguej624@gmail.com>
2019-04-15 16:22:59 -0700

    Add route for loader.io  + housekeeping

--- server/controller.js
+++ server/controller.js
@@ -1,17 +1,23 @@
 /* eslint-disable linebreak-style */
-const model = require('./model.js');
+const model = require("./model.js");
 
 module.exports = {
-  handleReadForAllSongs: (req, res) => {app.get('/loaderio-00072e857aba6340088297ab56ffd7c6/', (req, res) => { 
-    res.sendFile(path.resolve(__dirname, './loaderio-00072e857aba6340088297ab56ffd7c6.txt'));
-  }); 
+  handleReadForAllSongs: (req, res) => {
+    app.get("/loaderio-00072e857aba6340088297ab56ffd7c6/", (req, res) => {
+      res.sendFile(
+        path.resolve(
+          __dirname,
+          "./loaderio-00072e857aba6340088297ab56ffd7c6.txt"
+        )
+      );
+    });
     const { songId } = req.params;
     model.fetchAllSongs(songId, (err, data) => {
       if (err) {
         res.sendStatus(403);
       } else {
-        // Parse the data, only if the songId is in the redis cache 
-        if (typeof data === 'string') {
+        // Parse the data, only if the songId is in the redis cache
+        if (typeof data === "string") {
           data = JSON.parse(data);
         }
         res.send(data).status(200);
@@ -22,20 +28,19 @@ module.exports = {
     const { songId } = req.params;
     model.fetchNumberOfComments(songId, (err, data) => {
       if (err) {
-        console.log('Error fetching number of comments: ', err);
+        console.log("Error fetching number of comments: ", err);
         res.sendStatus(403);
       } else {
         res.status(200).send(data.rows[0].count);
       }
-    
     });
   },
   handleCreateComment: (req, res) => {
     const { body } = req;
     model.writeNewComment(body, (err, data) => {
       if (err) {
-        console.log('Error writing comment: ', err);
-        res.sendStatus(404); 
+        console.log("Error writing comment: ", err);
+        res.sendStatus(404);
       } else {
         res.sendStatus(200);
       }
@@ -45,7 +50,7 @@ module.exports = {
     const { message } = req.params;
     model.updateComment(message, (err, data) => {
       if (err) {
-        console.log('Error updating comment: ', err);
+        console.log("Error updating comment: ", err);
         res.sendStatus(404);
       } else {
         res.sendStatus(200);
@@ -56,13 +61,11 @@ module.exports = {
     const { newMessage, oldMessage } = req.params;
     model.deleteComment(newMessage, oldMessage, (err, data) => {
       if (err) {
-        console.log('Error deleting a comment: ', err);
+        console.log("Error deleting a comment: ", err);
         res.sendStatus(404);
       } else {
         res.sendStatus(200);
       }
     });
-  },
+  }
 };
-
-
